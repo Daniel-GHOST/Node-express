b9f6c7afd0e892229516d9d6aa5fcc4a76b62586
Daniel-GHOST <zS18003193@estudiantes.uv.mx>
2023-10-11 22:29:05 -0600

    created json proxy #1

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ app.get('/Interests', (req, res, next)=>{
 	res.sendfile("assets/interests.json")
 })
 
+app.get('/proxy', (req, res, next)=>{
+	res.sendfile("assets/1a_proxy_size.json")
+})
+
 app.listen(port,  () => 
 	console.log('listening on port ' + port
 ));
